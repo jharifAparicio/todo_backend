99d2a3fb379f2d5301a1ea717de819aaf70c5a73
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-12-18 19:46:20 -0400

    Agregar configuración de Prisma, scripts de inicio y estructura básica del servidor Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// rutas
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+// iniciar el servidor
+const PORT = Bun.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
